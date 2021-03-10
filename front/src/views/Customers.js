7c6b564d21f599654c85802ca5372ada21e0261e
andasulca <andasulca@users.noreply.github.com>
2021-03-10 09:41:46 +0200

    prettier plugin and placeholder button

--- front/src/views/Customers.js
+++ front/src/views/Customers.js
@@ -1,9 +1,12 @@
 import CustomerTable from "../components/CustomerTable";
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Customers() {
     return (
         <div>
             <CustomerTable />
+            <Button variant="dark"><Link to="/">Go somewhere</Link></Button>
         </div>
     )
 }
