259ef09fe081a4c253cada7dd8c1a649e1d574d0
andasulca <andasulca@users.noreply.github.com>
2021-03-24 09:44:26 +0200

    Link, header, react-router-dom

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,17 +1,18 @@
 import { Navbar, Nav, NavDropdown, Form, Button } from 'react-bootstrap'
 import logo from '../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div>
             <Navbar bg="light">
-                <Navbar.Brand href=""><img src={logo} alt="Logo" className="logo"/></Navbar.Brand>
+                <Navbar.Brand as={Link} to="/"><img src={logo} alt="Logo" className="logo"/></Navbar.Brand>
                     <Nav className="mr-auto">
-                        <Nav.Link href="/">Klienti</Nav.Link>
-                        <Nav.Link href="/boxes">Kastes</Nav.Link>
+                        <Nav.Link as={Link} to="/">Klienti</Nav.Link>
+                        <Nav.Link as={Link} to="/boxes">Kastes</Nav.Link>
                         <NavDropdown title="Uzlīmes" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="newlabels">Izdrukāt jaunus svītrkodus</NavDropdown.Item>
-                            <NavDropdown.Item href="labels">Izdrukāt esošus svītrkodus</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/newlabels">Izdrukāt jaunus svītrkodus</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/labels">Izdrukāt esošus svītrkodus</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Form inline>
