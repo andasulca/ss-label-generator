07b566b41462b868a21fd8418ceb97e9cd59e3fb
andasulca <andasulca@users.noreply.github.com>
2021-03-25 17:31:42 +0200

    react-csv

--- front/src/views/Test.js
+++ front/src/views/Test.js
@@ -3,16 +3,25 @@ import AxiosGet from "../axios/AxiosGet";
 import Loader from "../components/BeatLoader";
 import ToPrint from "../components/ToPrint"
 import InputField from "../components/common/InputField";
+import { CSVLink } from "react-csv";
+
+const csvData = [
+    ["firstname", "lastname", "email"],
+    ["Ahmed", "Tomi", "ah@smthing.co.com"],
+    ["Raed", "Labes", "rl@smthing.co.com"],
+    ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+  ];
 
 const Test = () => {
     const clients = AxiosGet('clients');
-    
+
 
     return (
         <div>
             {clients.loading ? <Loader /> : <Selectize data={clients.data}/>}
             <InputField label={"Uzlīmju skaits"} />
             <ToPrint />
+            <CSVLink data={csvData}>Download me</CSVLink>;
         </div>
     )
 }
