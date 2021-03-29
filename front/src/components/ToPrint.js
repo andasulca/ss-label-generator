0298093fecf75f3f6b08177d87e557c9fc00978b
andasulca <andasulca@users.noreply.github.com>
2021-03-29 23:02:15 +0300

    barcodes

--- front/src/components/ToPrint.js
+++ front/src/components/ToPrint.js
@@ -1,28 +1,31 @@
 import React from "react";
 import ReactToPrint from "react-to-print";
 import Barcode from "react-barcode";
+//import InputField from "../components/common/InputField";
+import TextField from "@material-ui/core/TextField";
 
 class ComponentToPrint extends React.Component {
   render() {
+
+    const startingValue = this.props.data.nextbox.nextbox;
+
+    const components = [];
+    for (
+      let j = startingValue;
+      j < startingValue + parseInt(this.props.data.topicBox);
+      j++
+    ) {
+      components.push(Barcode);
+    }
+    const componentsToRender = components.map((Component, i) => (
+      <Component key={i + startingValue} value={i + startingValue} />
+    ));
+
     return (
       <table>
-        <thead>
-          <th>
-            <Barcode value="12345678" />
-          </th>
-          <th>column 2</th>
-          <th>column 3</th>
-        </thead>
         <tbody>
           <tr>
-            <td>data 1</td>
-            <td>data 2</td>
-            <td>data 3</td>
-          </tr>
-          <tr>
-            <td>data 1</td>
-            <td>data 2</td>
-            <td>data 3</td>
+            <td>{componentsToRender}</td>
           </tr>
         </tbody>
       </table>
@@ -30,18 +33,42 @@ class ComponentToPrint extends React.Component {
   }
 }
 
-class Example extends React.Component {
+class ToPrint extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topicBox: null,
+      nextbox: this.props.nextbox[0],
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
     return (
       <div>
+        <TextField
+          name="topicBox"
+          value={this.state.topicBox}
+          onChange={this.handleChange}
+          label={"Uzlīmju skaits"}
+        />
         <ReactToPrint
           trigger={() => <button>Print this out!</button>}
           content={() => this.componentRef}
         />
-        <ComponentToPrint ref={(el) => (this.componentRef = el)} />
+        <ComponentToPrint
+          ref={(el) => (this.componentRef = el)}
+          data={this.state}
+        />
       </div>
     );
   }
 }
 
-export default Example;
+export default ToPrint;
--- front/src/views/Test.js
+++ front/src/views/Test.js
@@ -2,17 +2,18 @@ import Selectize from "../components/common/Selectize"
 import AxiosGet from "../axios/AxiosGet";
 import Loader from "../components/BeatLoader";
 import ToPrint from "../components/ToPrint"
-import InputField from "../components/common/InputField";
+//import InputField from "../components/common/InputField";
 import { CSVLink } from "react-csv";
 
 const Test = () => {
     const clients = AxiosGet('clients');
-
+    const nextbox = AxiosGet('nextbox');
+    
     return (
         <div>
             {clients.loading ? <Loader /> : <Selectize data={clients.data}/>}
-            <InputField label={"Uzlīmju skaits"} />
-            <ToPrint />
+            
+            {nextbox.loading ? <Loader /> : <ToPrint nextbox={nextbox.data}/>}
             {clients.loading ? <Loader /> : <CSVLink data={clients.data}>Download me</CSVLink>}
         </div>
     )
