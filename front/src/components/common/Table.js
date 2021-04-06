6b10f8da3708152c2c1f29fb56f41763be5aaa6d
andasulca <anda.sulca@accenture.com>
2021-04-06 10:26:43 +0300

    removed inline style

--- front/src/components/common/Table.js
+++ front/src/components/common/Table.js
@@ -3,7 +3,7 @@ import { DataGrid } from '@material-ui/data-grid';
 const Table = ({ rows, columns, pageSize }) => {
 
 	return (
-		<div style={{ height: 600, width: '100%' }}>
+		<div className="table">
 			<DataGrid rows={rows} columns={columns} pageSize={pageSize} />
 		</div>
 	);
